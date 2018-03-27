1eb22a7e3b4b6f3a9680a9c99bc15da21a95dda1
pash10g <pash10g@gmail.com>
2018-03-27 18:34:05 +0300

    WIP

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ class Block {
     this.data = data;
     this.nonce = 0;
     this.approvals = [];
+    this.approvedByMajority = false;
     this.hash = this.calculateHash();
   }
 
@@ -39,24 +40,31 @@ class Blockchain{
     }
 
 
-     startMining(transactions,client)
+     startPrduce(transactions,client,atlasConn)
     {
     //  var block_id = this.returnPromise(this.getLatestBlock,transactions).then(block => {block_id = block.index + 1;
+         const pipeline = [
+      { $match: {'fullDocument.owner_id'  : {  "$ne" : client.authedId() } } }
+
+    ];
+      var db = atlasConn.db('transactions');
+      const changeStream = db.collection('pending_blocks').watch(pipeline);
+
+
+
     var block_id = this.getLatestBlock().index + 1;
-      this.addBlock(new Block(block_id,"20/07/2017", client.authedId(),{ transactions: [block_id] }),transactions,client);
+      this.addBlock(new Block(block_id,"20/07/2017", client.authedId(),{ transactions: [block_id] }),transactions,client,changeStream);
 
 
     }
 
-    createAndMine(transactions,client) {
+    createAndPrduce(transactions,client,atlasConn) {
 
-          transactions.collection('blockchain').find({index: 0}).limit(1).execute().then(docs => {
-              console.log(JSON.stringify(docs));
+          transactions.collection('validChain').find({}).execute().then(docs => {
               if (docs.length > 0)
               {
-                console.log("Got Genesis block");
-                var genesisBlock=docs[0];
-                console.log(genesisBlock);
+                console.log("Got initial chian");
+                this.chain=docs[0].chain;
 
               }
               else {
@@ -67,9 +75,7 @@ class Blockchain{
                 this.chain.push(genesisBlock);
 
               }
-              this.chain.push(genesisBlock);
-              console.log(transactions);
-              this.startMining(transactions,client);
+              this.startPrduce(transactions,client,atlasConn);
           }
         ).catch(err => {
           console.error(err)
@@ -95,51 +101,73 @@ class Blockchain{
 
     }
 
-    addBlock(newBlock,transactions,client) {
-      console.log("Start Mining Block: " + newBlock.index + "...");
+    addBlock(newBlock,transactions,client,changeStream) {
+      console.log("Start Producing Block: " + newBlock.index + "...");
         newBlock.previousHash = this.getLatestBlock().hash;
         newBlock.mineBlock(this.difficulty);
-        console.log("insert Block into db:",newBlock)
+
+        changeStream.next(function(err, next) {
+
+              console.log("Found doc for :" + next.fullDocument.index);
+              // Since changeStream has an implicit seession,
+              // we need to close the changeStream for unit testing purposes
+              //changeStream.close();
+              //client.close();
+              if ((next.fullDocument.index == newBlock.index))
+              {
+                if (client.authedId() == next.fullDocument.owner_id){
+
+                     console.log("The received notification was about block: " + next.fullDocument.index + " which was mined by us: " + next.fullDocument.owner_id);
+                }
+              }
+              else {
+                   console.log("The received notification is about historical block: " + next.fullDocument.index + " which was mined by: " + next.fullDocument.owner_id);
+                   console.log("About toi validate block " + JSON.stringify(next));
+                   this.verifyBlock(next.fullDocument,transactions,client);
+              }
+
+    });
+
         try {
           var coll = transactions.collection("pending_blocks");
           coll.insertOne(newBlock).then(() =>
-          {  this.chain.push(newBlock);
-             var block_id = newBlock.index + 1;
-             this.addBlock(new Block(block_id,new Date(), client.authedId(),{ transactions: [block_id] }),transactions,client);
-             console.log("Finished Mining block #n: " + newBlock.index  + " HASH: " + newBlock.hash  )  ;
+          {
+            this.chain.push(newBlock);
+            var block_id = newBlock.index + 1;
+            setTimeout(this.addBlock.bind(this),100,new Block(block_id,new Date(), client.authedId(),{ transactions: [block_id] }),transactions,client,changeStream);
+             console.log("Finished producing block #n: " + newBlock.index  + " HASH: " + newBlock.hash  )  ;
            }
           ).catch(err => {
-          console.error(err)
+
+            transactions.collection('validChain').find({}).execute().then(docs => {
+                if (docs.length > 0)
+                {
+                  this.chain=docs[0].chain;
+                  var block_id = this.getLatestBlock().index + 2;
+                  setTimeout(this.addBlock.bind(this),100,new Block(block_id,new Date(), client.authedId(),{ transactions: [block_id] }),transactions,client,changeStream);
+                  console.log("Block #n: " + newBlock.index  + " was mined by another node : " + this.getLatestBlock().owner_id +" Moving to the next one: " + block_id )  ;
+                }
+              });
         });
       }
-        catch (e) {
-        // handle write error
-        console.log(e.message);
-        return e.message;
-      }
+      catch (e) {
+      // handle write error
+      console.error(e.message)
     }
+  }
 
-    getLatestChain(transactions)
-    {
-           return transactions.collection('validChain').find({}).execute();
-   }
 
-    isChainValid(transactions) {
-        this.chain = this.getLatestChain();
-        for (let i = 1; i < this.chain.length; i++){
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
 
-            if (currentBlock.hash !== currentBlock.calculateHash()) {
+    verifyBlock(block,transactions,client) {
+           console.log("Verify block");
+            tempBlock = new Block(block.index,block.timesamp, block.owner_id,block.data);
+
+            if (block.hash !== tempBlock.calculateHash()) {
                 return false;
             }
-
-            if (currentBlock.previousHash !== previousBlock.hash) {
-                return false;
-            }
-        }
-
-        return true;
+            block.approvals.push(client.authedId());
+            transactions.collection('pending_blocks').updateOne({index: block.index},{$set: block}).then(()=>{
+            return true;});
     }
     /* transactions.collection('pending_blocks').aggregate({$graphLookup: { from: "pending_blocks" , startWith: "$previousHash", connect
 
@@ -161,10 +189,17 @@ clientPromise.then(client => {
    nodes = assets.collection('nodes');
    nodes.updateOne({owner_id: client.authedId()}, {$set: miner}, {upsert:true});
 
-  MongoDBCoin = new Blockchain(transactions);
-  transactions.collection("pending_blocks").deleteMany({}).then(() => {MongoDBCoin.createAndMine(transactions,client);}).catch(err => {
-  console.error(err)
-});
+
+    var uri = "mongodb://blockadmin:block123@blockchaindb-shard-00-00-vjsde.mongodb.net:27017,blockchaindb-shard-00-01-vjsde.mongodb.net:27017,blockchaindb-shard-00-02-vjsde.mongodb.net:27017/transactions?ssl=true&replicaSet=BlockchainDB-shard-0&authSource=admin";
+    MongoClient.connect(uri, function(err, db) {
+      console.log("#########################################");
+      console.log("####  Node: " + client.authedId() + " is up and starting to produce at : " + new Date());
+      console.log("#########################################");
+       MongoDBCoin = new Blockchain(transactions);
+       MongoDBCoin.createAndPrduce(transactions,client,db);
+    });
+
+
 
 });
 
